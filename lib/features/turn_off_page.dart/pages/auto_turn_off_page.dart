620578793eb565923f9003dee8b1273ce91d6084
Alif8454 <malif201106@gmail.com>
2024-06-18 12:28:56 +0700

    menambahkan padding pada title auto turn off dan toggle

--- lib/features/turn_off_page.dart/pages/auto_turn_off_page.dart
+++ lib/features/turn_off_page.dart/pages/auto_turn_off_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AutoTurnOffPage extends StatelessWidget {
+class AutoTurnOffPage extends StatefulWidget {
   const AutoTurnOffPage({super.key});
+
+  @override
+  State<AutoTurnOffPage> createState() => _AutoTurnOffPageState();
+}
+
+class _AutoTurnOffPageState extends State<AutoTurnOffPage> {
+  bool _isAutoTurnOffEnabled = false;
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +16,29 @@ class AutoTurnOffPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Auto Turn Off Page'),
       ),
-      body: const Center(
-        child: Text('Auto Turn Off Page'),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(right: 10), // Add padding to the right
+              child: Text('Auto Turn Off'),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 10), // Add padding to the left
+              child: Switch(
+                value: _isAutoTurnOffEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    _isAutoTurnOffEnabled = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
